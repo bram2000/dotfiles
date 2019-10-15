c03729acfb5f5f6f77fbe3c7d5598ebad84ecaa6
Jon Bramley <jon.bramley@sainsburys.co.uk>
2019-10-15 14:22:35 +0100

    Initial commit of fish configs

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set theme_color_scheme gruvbox
+
+. ~/.asdf/asdf.fish
+
+# Proxy required for Sainsbury's
+proxy_on
