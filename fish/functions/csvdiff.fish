45b4422b6ead65cad0eeb4f82c5edc6907a5e290
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-08-28 13:30:39 +0100

    Take optional list of columns to diff

--- fish/functions/csvdiff.fish
+++ fish/functions/csvdiff.fish
@@ -1,7 +1,14 @@
-function csvdiff --argument file1 file2
-	if [ (count $argv) -ne 2 ]
+function csvdiff --argument file1 file2 columns
+set cut_arg ""
+if [ (count $argv) -eq 3 ]
+set cut_arg -c "$columns"
+else if [ (count $argv) -ne 2 ]
 echo "expected exactly two files to compare"
 return 1
 end
-vimdiff (cat $file1 | csvlook -I | psub) (cat $file2 | csvlook -I | psub)
+if [ "$cut_arg" = "" ]
+eval vimdiff (cat $file1 | csvlook -I | psub) (cat $file2 | csvlook -I | psub)
+else
+eval vimdiff (cat $file1 | csvcut $cut_arg | csvlook -I | psub) (cat $file2 | csvcut $cut_arg | csvlook -I | psub)
+end
 end
